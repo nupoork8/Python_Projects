a34af914bf320ae94a05dc618e1fa7063c0ab538
nupoor kedare <nupoorktech@gmail.com>
2025-12-10 22:04:15 +0530

    Add files via upload

---
+++ email_slicer.py
@@ -0,0 +1,8 @@
+# Basic email slicing programme
+
+email = input("enter your email:")
+# index = email.index("@")
+username = email[:email.index("@")]
+domain = email[email.index("@") + 1 :]
+
+print(f"your username is : {username} and domain is : {domain}")
