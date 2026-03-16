2c6608b45e495a404aa687f572d4ae886733e23f
nupoor kedare <nupoorktech@gmail.com>
2026-03-16 12:00:52 +0530

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,19 @@
+# string manipulation 
+#print("Hello World\nHello World\nHello world")
+#print("Hello"  +  " " + "nupoor")
+
+#input function : input("a prompt for user")
+#print("Hello" + " " + input("enter your name"))
+
+# -- BAND NAME GENERATOR PROJECT --
+# 1. Create a greeting for your program
+# 2. ask the user for the city that they grw up in and store it in a variable
+# 3. ask the user for the name of pet and store it in a variable
+# 4 . combine the name of their city and pet and show them their band name
+# 5. make sure the input cursor shows on the new line
+
+print("-------WELCOME TO BAND NAME GENERATOR PROGRAM----------")
+
+city =input("What's the name of the city you grew up in ?  ")
+pet = input("Wwhat is the name of your pet ?  ")
+print(f"Your band name could be :  " + city + pet )
